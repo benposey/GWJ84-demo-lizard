ab7f1c68580be646e87d0e6524196b3f413163a9
benposey <benposey.dev@gmail.com>
2025-08-10 18:36:57 -0400

    box explosion vfx and sound

--- objects/box.gd
+++ objects/box.gd
@@ -2,17 +2,28 @@ class_name Box
 extends RigidBody2D
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var small_explosion: Node2D = $SmallExplosion
+
+var is_destroyed: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	small_explosion.hide()
 
 func destroy():
-	animation_player.play("explode")
-	await(animation_player.animation_finished)
-	queue_free()
+	if !is_destroyed:
+		is_destroyed = true
+		animation_player.play("explode")
+		#play_small_explosion()
+		await(animation_player.animation_finished)
+		queue_free()
+
+func play_small_explosion():
+	var rand_scale = randf_range(.8, 1.2)
+	small_explosion.scale = Vector2(rand_scale, rand_scale)
+	small_explosion.show()
+	for sprite in small_explosion.get_children():
+		if sprite is AnimatedSprite2D:
+			var rand_rotation = randi_range(0,6)
+			sprite.rotate(rand_rotation)
+			sprite.play("default")
