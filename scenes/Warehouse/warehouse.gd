421fca943e6a696640f80017d71d21f5f520036a
John Dott <jkd28@pitt.edu>
2025-08-10 22:37:59 -0400

    Refactor UI elements in tree structure (#14)
    * Refactor ui files for organization
    
    * Implement static point counter in UI

--- scenes/Warehouse/warehouse.gd
+++ scenes/Warehouse/warehouse.gd
@@ -1,11 +1,9 @@
 extends Node2D
 
-
-@onready var objects: Node2D = $Objects
-
 func _input(event):
 	if event.is_action_pressed("detonate"):
 		for box in get_tree().get_nodes_in_group("destructable"):
+			await get_tree().create_timer(1.0).timeout
 			if box is Box:
 				box.destroy()
 	
