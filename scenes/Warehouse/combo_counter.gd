73147bd45a0b5c585c2dc00e5e14c52b0c9629ef
John Dott <jkd28@pitt.edu>
2025-08-11 23:30:17 -0400

    Add timer countdown and combo text

--- scenes/Warehouse/combo_counter.gd
+++ scenes/Warehouse/combo_counter.gd
@@ -1,13 +1,18 @@
 extends RichTextLabel
 
 var current_combo = 1
+const TEXT_FORMAT = "[color=%s]%s[/color] "
 
 func _ready() -> void:
 	self.text = str(current_combo) + "x"
 	Events.objects.object_destroyed.connect(_on_object_destroyed)
+	Events.combos.combo_changed.connect(_on_combo_multiplier_changed)
 	
 func _on_object_destroyed(_point_value: int, _spawn_position: Vector2) -> void:
 	if not current_combo >= 8:
 		current_combo *= 2
-		self.text = str(current_combo) + "x"
 		Events.combos.combo_changed.emit(current_combo)
+		
+func _on_combo_multiplier_changed(new_multiplier) -> void:	
+	self.text = TEXT_FORMAT % [GlobalVars.Multiplier_To_Color[new_multiplier], str(new_multiplier) + "x"]
+	
--- scenes/Warehouse/warehouse.gd
+++ scenes/Warehouse/warehouse.gd
@@ -1,9 +1,18 @@
 extends Node2D
+@onready var combo_bar: ProgressBar = $FixedUI/ComboBar
+@onready var combo_timer: Timer = $FixedUI/ComboTimer
+
+const DEFAULT_COMBO = 1
+
+func _process(delta: float) -> void:
+	combo_bar.value = combo_timer.time_left
 
 func _input(event):
 	if event.is_action_pressed("restart"):
 		get_tree().reload_current_scene()
 
-
 func _on_pause_button_pressed() -> void:
 	Events.menus.paused.emit()
+	
+func _on_combo_timer_expired() -> void:
+	Events.combos.combo_changed.emit(DEFAULT_COMBO)
