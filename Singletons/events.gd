2bc81a8d2ce5680a20495a8d03a92e54fe3f6941
John Dott <jkd28@pitt.edu>
2025-08-11 22:41:00 -0400

    Add basic combo system (#22)

--- Singletons/events.gd
+++ Singletons/events.gd
@@ -2,9 +2,13 @@ extends Node
 
 var objects = ObjectEvents.new()
 var menus = MenuEvents.new()
+var combos = ComboEvents.new()
 
 class ObjectEvents:
-	signal object_destroyed(point_value, position)
+	signal object_destroyed(point_value: int, position:Vector2)
 
 class MenuEvents:
 	signal paused()
+	
+class ComboEvents:
+	signal combo_changed(new_combo: int)
--- scenes/Warehouse/warehouse.gd
+++ scenes/Warehouse/warehouse.gd
@@ -1,12 +1,6 @@
 extends Node2D
 
 func _input(event):
-	if event.is_action_pressed("detonate"):
-		for box in get_tree().get_nodes_in_group("destructable"):
-			await get_tree().create_timer(1.0).timeout
-			if box is Box:
-				box.destroy()
-	
 	if event.is_action_pressed("restart"):
 		get_tree().reload_current_scene()
 
