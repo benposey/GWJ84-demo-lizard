3a023bd460775eef5f67a3241437d3525461feb8
koiboiroi <klabundification@gmail.com>
2025-08-11 22:02:06 -0400

    QTE refactor + pseudo code
    created LevelEvents and level_end signal. refactored qte code and added some commented pseudo/ todo

--- Singletons/events.gd
+++ Singletons/events.gd
@@ -2,9 +2,13 @@ extends Node
 
 var objects = ObjectEvents.new()
 var menus = MenuEvents.new()
+var level = LevelEvents.new()
 
 class ObjectEvents:
 	signal object_destroyed(point_value, position)
 
 class MenuEvents:
 	signal paused()
+
+class LevelEvents:
+	signal level_end()
