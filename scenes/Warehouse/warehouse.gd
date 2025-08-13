5a170ec81cec0c98847eb50b09d77e17ceb260a2
koiboiroi <klabundification@gmail.com>
2025-08-12 21:27:10 -0400

    re-added end of level logic
    re-added end of level logic post-overhaul of object creation

--- scenes/Warehouse/warehouse.gd
+++ scenes/Warehouse/warehouse.gd
@@ -1,12 +1,15 @@
 extends Node2D
 @onready var combo_bar: ProgressBar = $FixedUI/ComboBar
 @onready var total_object_destroyed_count = 0
+@onready var spawnables: TileMapLayer = $LayerHolder/Spawnables
+
+var destroyable_count = 0
 
 const DEFAULT_COMBO = 1
 
 func _ready() -> void:
 	Events.objects.object_destroyed.connect(_on_object_destroyed)
-
+	destroyable_count = spawnables.get_child_count()
 
 func _input(event):
 	if event.is_action_pressed("restart"):
@@ -18,11 +21,10 @@ func _on_object_destroyed(_point_value, _position):
 	var time_remaining = combo_bar.time_left
 	combo_bar.start(time_remaining + 1.5)
 	
-	# Check level ending conditions
-	#total_object_destroyed_count += 1	
-	#if total_object_destroyed_count == child_count:
-		#Events.level.level_ended.emit()
-		#print("level ended")
+	total_object_destroyed_count += 1	
+	if total_object_destroyed_count == destroyable_count:
+		Events.level.level_ended.emit()
+		print("level ended")
 	
 
 func _on_pause_button_pressed() -> void:
