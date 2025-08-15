7f9925ad56cefdd00578ed6c36e5570ea67d7138
koiboiroi <klabundification@gmail.com>
2025-08-14 23:47:27 -0400

    qte timer bar + sizing cleanup
    added timer bar, resized lizard and button. cleaned up code.

--- scenes/QuickTime/level_end_qte.gd
+++ scenes/QuickTime/level_end_qte.gd
@@ -5,20 +5,18 @@ extends Node2D
 @onready var lizard_sound: AudioStreamPlayer = $lizardSound
 @onready var qte_timer: Timer = $QTE_Timer
 @onready var instructions: RichTextLabel = $Instructions
+@onready var time_bar_for_qte: ProgressBar = $time_bar_for_qte
 
 var level_completion_time_sec = 0.0
 var level_completion_objects_destroyed = 0
 var player_score = 0
-
-var buttonPressCount = 0
-var quickEventTime = 5.0 #seconds
+var button_press_count = 0.0
 var incrementRate = 0.1
 var qte_multiplier = 0.0
 
-#todo implement quick time event popping up and timer starting
-#func EndOfLevelTrigger(event):
-	#display QTEd
-	#var quickTimeEventTimer = get_tree().create_timer(quickEventTime)
+func _process(delta: float) -> void:
+	time_bar_for_qte.value = qte_timer.time_left
+	print("time_bar_for_qte ", time_bar_for_qte.value)
 
 func _on_end_of_level(score: int, level_complete_time_sec: float, objects_destoryed: int):
 	# passthrough variables for end game statistics 
@@ -29,19 +27,22 @@ func _on_end_of_level(score: int, level_complete_time_sec: float, objects_destor
 	# QTE logic
 	self.show()	
 	instructions.show()
-	await get_tree().create_timer(3.0).timeout
+	await get_tree().create_timer(1.0).timeout
 	instructions.hide()
-	buttonPressCount = 1
+	button_press_count = 1
 	qte_timer.start()
-	await qte_timer.timeout
+	
+	#todo add multiplier counter
+	#todo add shake juice
+	#todo add color juice
 	
 func _input(event):
 	if event.is_action_pressed("detonate"):
 		lizard_qte.play("LizardJump")
 		red_button.play("RedButtonPressed")
-		lizard_qte.speed_scale = buttonPressCount
-		red_button.speed_scale = buttonPressCount
-		buttonPressCount += incrementRate
+		lizard_qte.speed_scale = button_press_count
+		red_button.speed_scale = button_press_count
+		button_press_count += incrementRate
 		lizard_sound.play()
 		
 		qte_multiplier = 50.0 #todo increment score multiplier
