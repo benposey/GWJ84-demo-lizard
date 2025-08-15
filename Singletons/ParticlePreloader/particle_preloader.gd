024cbe69ed32bafbb16df3109d3646d83cc9651f
John Dott <jkd28@pitt.edu>
2025-08-15 00:31:23 -0400

    Don't play sound in box when preloading

--- Singletons/ParticlePreloader/particle_preloader.gd
+++ Singletons/ParticlePreloader/particle_preloader.gd
@@ -1,5 +1,7 @@
 extends Node2D
-@onready var box: Box = $Box
+@onready var box: Box = $CanvasLayer/Box
+@onready var color_rect: ColorRect = $CanvasLayer/ColorRect
 
 func _ready() -> void:
+	
 	box.destroy()
